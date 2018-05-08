6e93145899ab95d69474bc5fff944d8942cb543e
Sabine Gruber <sab@vollbio.de>
2018-05-08 16:03:03 +0200

    try intermediate exercise

---
+++ main2.js
@@ -0,0 +1,53 @@
+var apple = {
+	name : "apple",
+	price : 2,
+	count : 1 
+}
+
+var arr = [];
+function addtocart(){
+	arr = arr.push(apple);
+}
+
+var banana = {
+	name : "banana",
+	price : 3,
+	count : 1 
+}
+
+var arr = [];
+function addtocart(){
+	arr = arr.push(banana);
+}
+
+var orange = {
+	name : "orange",
+	price : 3,
+	count : 1 
+}
+
+var arr = [];
+function addtocart(){
+	arr = arr.push(orange);
+}
+
+var lemon = {
+	name : "lemon",
+	price : 2,
+	count : 1 
+}
+
+var arr = [];
+function addtocart(){
+	arr = arr.push(lemon);
+}
+
+$(document).ready(function() {
+/*$("banana[]").click(){
+	(this).addtocart()
+} */
+
+$("arr").eq(1);
+
+
+});
